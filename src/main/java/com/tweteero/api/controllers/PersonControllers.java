4010f5d5a6c2fc38a638d4a50bae4ca4825b973c
ricardomartinso <ricardomartinso.dev@gmail.com>
2023-01-24 18:18:44 -0300

    feat: added Cors permission to all

--- src/main/java/com/tweteero/api/controllers/PersonControllers.java
+++ src/main/java/com/tweteero/api/controllers/PersonControllers.java
@@ -3,6 +3,7 @@ package com.tweteero.api.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,7 +20,8 @@ import com.tweteero.api.services.PersonService;
 import jakarta.validation.Valid;
 
 @RestController
-@RequestMapping("/users")
+@RequestMapping()
+@CrossOrigin(origins = "*")
 public class PersonControllers {
 
     @Autowired
